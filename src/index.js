6446c275aa7cd81b54d21ba7ffcfdd28a4938a56
JinCheng <JinChengJoker@foxmail.com>
2019-09-19 17:46:05 +0800

    optimized code

--- src/index.js
+++ src/index.js
@@ -1,38 +1,23 @@
 function deepClone(resource) {
   if (resource instanceof Object) {
+    let result
     if (resource instanceof Array) {
-      const result = new Array()
-      for (let key in resource) {
-        result[key] = deepClone(resource[key])
-      }
-      return result
+      result = new Array()
     } else if (resource instanceof Function) {
-      const result = function () {
+      result = function () {
         return resource.apply(this, arguments)
       }
-      for (let key in resource) {
-        result[key] = deepClone(resource[key])
-      }
-      return result
     } else if (resource instanceof RegExp) {
-      const result = new RegExp(resource.source, resource.flags)
-      for (let key in resource) {
-        result[key] = deepClone(resource[key])
-      }
-      return result
+      result = new RegExp(resource.source, resource.flags)
     } else if (resource instanceof Date) {
-      const result = new Date(resource)
-      for (let key in resource) {
-        result[key] = deepClone(resource[key])
-      }
-      return result
+      result = new Date(resource)
     } else {
-      const result = new Object()
-      for (let key in resource) {
-        result[key] = deepClone(resource[key])
-      }
-      return result
+      result = new Object()
     }
+    for (let key in resource) {
+      result[key] = deepClone(resource[key])
+    }
+    return result
   }
   return resource
 }
